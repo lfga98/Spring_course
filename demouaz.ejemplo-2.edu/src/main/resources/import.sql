679f3ff021df14a84bc90fc5f748f3f851f8544a
lfga98 <lfgarciauaz@gmail.com>
2019-01-09 14:21:12 -0600

    Added more clients

--- demouaz.ejemplo-2.edu/src/main/resources/import.sql
+++ demouaz.ejemplo-2.edu/src/main/resources/import.sql
@@ -1,2 +1,6 @@
-INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28', 'hola.jpg');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28', '');
 INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(3, 'Luis', 'Garcia', 'lgarcia@gmail.com', '2018-07-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(4, 'Leo', 'Garcia', 'leogarcia@gmail.com', '2018-05-22', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(5, 'Reina', 'Castillo', 'rCastillo@gmail.com', '2019-04-12', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(6, 'Takumi', 'Mikamoto', 'takumi@gmail.com', '2010-02-12', '');

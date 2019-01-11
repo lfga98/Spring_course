ee16d789a66600c56f2b7b9441c18560b55318e2
lfga98 <lfgarciauaz@gmail.com>
2019-01-11 17:09:42 -0600

    Created Entity classes

--- demouaz.ejemplo-2.edu/src/main/java/uaz/ejemplo/edu/demouaz/ejemplo/edu/models/entity/Factura.java
+++ demouaz.ejemplo-2.edu/src/main/java/uaz/ejemplo/edu/demouaz/ejemplo/edu/models/entity/Factura.java
@@ -2,7 +2,9 @@ package uaz.ejemplo.edu.demouaz.ejemplo.edu.models.entity;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "facturas")
@@ -24,6 +26,26 @@ public class Factura implements Serializable {
 
     @ManyToOne
     private Cliente cliente;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "factura_id")
+    private List<ItemFactura> items;
+
+
+    public Factura(){
+        items=new ArrayList<>();
+    }
+
+    public void addItemFactura(ItemFactura item){
+        items.add(item);
+    }
+    public List<ItemFactura> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemFactura> items) {
+        this.items = items;
+    }
 
     @PrePersist
     public void prePersist(){
@@ -60,5 +82,14 @@ public class Factura implements Serializable {
 
     public void setCreateAt(Date createAt) {
         this.createAt = createAt;
+    }
+
+    public Double getTotal(){
+        Double total=0.0;
+        int size = items.size();
+        for (int i=0;i<size;i++){
+            total+= items.get(i).calcularImporte();
+        }
+        return total;
     }
 }
